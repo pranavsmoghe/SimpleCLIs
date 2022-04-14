20be8ebdcb0c91298d76f44178338181e4cab692
Pranav Moghe <35550633+pranavsmoghe@users.noreply.github.com>
2022-04-14 22:53:39 +0530

    Update fire.py

--- fire.py
+++ fire.py
@@ -1,5 +1,10 @@
 import fire
 
+'''
+This is a simple Fire based CLI which gives the sum of the first x natural numbers or squares x depending
+upon the argument passed. This is a slight modification of one of their own examples, provided in their documentation.
+'''
+
 def addupto(x):
   return (x(x+1))/2
 
